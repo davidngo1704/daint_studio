2d657e6eec1da62664a811285060c011c9c0598c
dai <davidngo1704@gmail.com>
2026-05-25 14:48:01 +0700

    ok

--- DaintStudio/UserControls/Main/GiaoTiepUserControl.cs
+++ DaintStudio/UserControls/Main/GiaoTiepUserControl.cs
@@ -5,11 +5,14 @@ using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
+using NAudio.Wave;
 
 namespace DaintStudio.UserControls.Main
 {
     public partial class GiaoTiepUserControl : UserControl
     {
+        private WaveInEvent waveSource;
+        private WaveFileWriter waveFile;
         public GiaoTiepUserControl()
         {
             InitializeComponent();
@@ -17,7 +20,40 @@ namespace DaintStudio.UserControls.Main
 
         private void button1_Click(object sender, EventArgs e)
         {
+            waveSource.StopRecording();
+        }
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            waveSource = new WaveInEvent();
 
+            waveSource.WaveFormat = new WaveFormat(44100, 1);
+
+            waveSource.DataAvailable += WaveSource_DataAvailable;
+            waveSource.RecordingStopped += WaveSource_RecordingStopped;
+
+            waveFile = new WaveFileWriter("record.wav", waveSource.WaveFormat);
+
+            waveSource.StartRecording();
+
+            MessageBox.Show("Bắt đầu thu âm");
+        }
+        private void WaveSource_DataAvailable(object sender, WaveInEventArgs e)
+        {
+            if (waveFile != null)
+            {
+                waveFile.Write(e.Buffer, 0, e.BytesRecorded);
+                waveFile.Flush();
+            }
+        }
+        private void WaveSource_RecordingStopped(object sender, StoppedEventArgs e)
+        {
+            waveSource.Dispose();
+            waveSource = null;
+
+            waveFile.Dispose();
+            waveFile = null;
+
+            MessageBox.Show("Đã lưu file record.wav");
         }
     }
 }
